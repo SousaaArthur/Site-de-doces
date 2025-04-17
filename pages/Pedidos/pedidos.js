651ce61231bb6cc47229d026cbddae092f99d2d6
SousaaArthur <arthursousaa2004@gmail.com>
2025-04-16 23:07:44 -0300

    Adicionando api de clima e exibição do clima ao colocar ao cep

--- pages/Pedidos/pedidos.js
+++ pages/Pedidos/pedidos.js
@@ -2,6 +2,13 @@ const inputCep = document.getElementById('txtCEP');
 const inputStreet = document.getElementById('txtStreet');
 const inputNeighborhood = document.getElementById('txtNeighborhood');
 const inputState = document.getElementById('txtState');
+const inputCity = document.getElementById('txtCity');
+
+const climateCity = document.getElementById('climateCity');
+const maximumTemp = document.getElementById('maximumTemp');
+const aboutClimate = document.getElementById('aboutClimate');
+
+const floatingScreen = document.querySelector('.floating-screen');
 
 inputCep.addEventListener('change', () => {
   const cepValue = inputCep.value;
@@ -23,7 +30,33 @@ inputCep.addEventListener('change', () => {
         inputStreet.value = dados.logradouro || '';
         inputNeighborhood.value = dados.bairro || '';
         inputState.value = dados.estado || '';
+        inputCity.value = dados.localidade || '';
 
+        let city = dados.localidade;
+
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${CONFIG.API_KEY}&q=${city}&lang=pt`)
+        .then(response => {
+          if(!response.ok) {
+            throw new Error('Erro ao fazer a requisição da Weather API');
+          }
+          return response.json();
+        })
+        .then(dados => {
+          if (dados.erro){
+            alert("Cidade não encontrada.");
+            return
+          }
+      
+          climateCity.innerHTML = dados.location.region;
+          maximumTemp.innerHTML = dados.current.temp_c;
+          aboutClimate.innerHTML = dados.current.condition.text;
+      
+          console.log(dados.current.temp_c);
+          floatingScreen.classList.remove("hidden");
+        })
+        .catch(erro => {
+          console.error('Erro:', erro)
+        });
       })
       .catch(erro => {
         console.error('Deu erro:', erro);
@@ -32,3 +65,7 @@ inputCep.addEventListener('change', () => {
     console.error('CEP não pode estar vazio');
   }
 });
+
+document.querySelector('.btn-close').addEventListener('click', () => {
+  floatingScreen.classList.add("hidden");
+})
