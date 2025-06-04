c43b4f3fe03904f61972987d7036b12b29f45e86
SousaaArthur <arthursousaa2004@gmail.com>
2025-06-03 22:40:36 -0300

    Feito o botao de adicionar ao carrinho

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ document.querySelectorAll('.carrosel').forEach(carrosel => {
 function cookieHidden() {
   cookie.style.display = 'none';
 }
+
+const cart = document.querySelector('.products-list'); 
+const cartCount = document.getElementById('cart-count');
+
+document.querySelectorAll('.btn-card button').forEach(button => {
+  button.addEventListener('click', () => {
+    const card = button.closest('.card');
+    const productName = card.querySelector('.description-card h3').textContent;
+
+    const product = document.createElement('div');
+    product.classList.add('product');
+    product.innerHTML = `
+      <i class="fa-solid fa-image fa-3x" style="color: #B197FC;"></i>
+      <p>${productName}</p>
+      <i class="fa-solid fa-trash-can trash-btn"></i>
+    `;
+    const trashBtn = product.querySelector('.trash-btn');
+    trashBtn.addEventListener('click', () => {
+      product.remove();
+      const products = cart.querySelectorAll('.product');
+      cartCount.textContent = products.length;
+    });
+
+    cart.appendChild(product);
+
+  const currentCount = parseInt(cartCount.textContent, 10) || 0;
+  cartCount.textContent = currentCount + 1;
+
+  });
+});
